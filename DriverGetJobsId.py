1aa5c2843862562f6836350cf020673f2486576a
ChonJi <remigiusz.chomicki@gmail.com>
2019-01-22 11:22:21 +0100

    Initial commit

--- DriverGetJobsId.py
+++ DriverGetJobsId.py
@@ -4,21 +4,50 @@ from selenium import webdriver
 class DriverGetJobsId():
     __page__ = 'https://gitlab.com/ChonJi1983/TestRun/-/jobs'
 
-    def test_build_job_id_list(self):
+    def build_job_id_list(self):
 
         list_artifacts = []
 
-        driver = webdriver.Chrome(executable_path=r"driver\chromedriver.exe")
+        self.driver = webdriver.Chrome(executable_path=r"driver\chromedriver.exe")
+        driver = self.driver
         driver.maximize_window()
         driver.get(self.__page__)
 
-        list_of_stages = driver.find_elements_by_xpath('//tbody/tr/td[4]')
+        list_of_stages = self.get_stages_list()
 
         for index, stage in enumerate(list_of_stages):
 
-            if driver.find_element_by_xpath(f"//tbody/tr[{index + 1}]/td[4]").text == 'test':
-                element = driver.find_element_by_xpath(f"(//tbody//span[@class = 'build-link'])[{index + 1}]").text[1:]
-                list_artifacts.append(element)
+            if self.get_stage_by_row(index + 1) == 'test' and self.get_status_by_row(index + 1) not in (
+                    'canceled', 'running'):
+                list_artifacts.append(self.get_job_id_by_row(index + 1))
 
         driver.quit()
         return list_artifacts
+
+    def get_stages_list(self):
+        return self.driver.find_elements_by_xpath('//tbody/tr/td[4]')
+
+    def get_stage_by_row(self, row):
+        stage_element = self.driver.find_element_by_xpath(f'//tbody/tr[{row}]/td[4]')
+        return stage_element.text
+
+    def get_status_by_row(self, row):
+        status_element = self.driver.find_element_by_xpath(f'//tbody/tr[{row}]/td[1]')
+        return status_element.text
+
+    def get_job_id_by_row(self, row):
+        jobid_element = self.driver.find_element_by_xpath(f"(//tbody//span[@class = 'build-link'])[{row}]")
+        return jobid_element.text[1:]
+
+    def get_pipeline_by_row(self, row):
+        pipeline_element = self.driver.find_element_by_xpath(f'//tbody/tr[{row}]/td[3]')
+        return pipeline_element.text
+
+    def get_name_by_row(self, row):
+        name_element = self.driver.find_element_by_xpath(f'//tbody/tr[{row}]/td[5]')
+        return name_element.text
+
+    # Gets job branch name
+    def get_refname_by_row(self, row):
+        refname_element = self.driver.find_element_by_xpath(f'(//a[@class="ref-name"])[{row}]')
+        return refname_element.text
